204fcc672d471d4a90554b0cfbe7a0223c919d88
Souta Ito <soutaito@info-lounge.jp>
2014-08-29 09:53:01 +0900

    フォームでグリッド表示のチェックボックスにheightline

--- library/superform/view/element/checkboxescustom.html.php
+++ library/superform/view/element/checkboxescustom.html.php
@@ -3,7 +3,7 @@
 <?php if(empty($checkbox['category'])) : ?>
 <?php if($key != 0) : ?></ul><?php endif;?>
 <p><?php echo $checkbox->label;?></p>
-<ul>
+<ul class="heightLineParent">
 <?php else : ?>
     <li><?php echo $checkbox->getInnerHTML() ?></li>
 <?php endif; ?>
