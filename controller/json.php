16862df42b22bffc1bd877ad3b5d5b1d9cf44fbc
Souta Ito <soutaito@info-lounge.jp>
2015-01-12 14:58:52 +0900

    Remove wrong args

--- controller/json.php
+++ controller/json.php
@@ -288,7 +288,7 @@ namespace Goteo\Controller {
          * @throws \Goteo\Core\Exception
          */
         function get_pickup_projects(){
-            $_result = Promote::getAll(true);
+            $_result = Promote::getAll();
             if (count($_result) > 3) {
                 for ($i = 0; $i < 3; $i++) {
                     $this->result[] = $_result[$i];
