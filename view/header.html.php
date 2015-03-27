f11d74d2fbea6f46734c469997456285ecbfbfc2
Naoko Usui <usui@info-lounge.jp>
2015-03-27 18:52:04 +0900

    fix path for earth_view button

--- view/header.html.php
+++ view/header.html.php
@@ -89,7 +89,7 @@ use Goteo\Library\Text,
                 このまち、わたしから未来を創る
             </div>
         </div>
-        <a class="earth_view" href="http://yokohama.mapping.jp/" target="_blank"><img src="/view/css/header/i.png" alt="Earth View" /></a>
+        <a class="earth_view" href="http://yokohama.mapping.jp/" target="_blank"><img src="/view/css/header/earth_view_icon.png" alt="Earth View" /></a>
     </div>
     <div class="nav_wrapper">
         <div class="nav_inner">
