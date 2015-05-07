d290c60c56c91f227957570329393c0f7dee7dc7
Naoko Usui <usui@info-lounge.jp>
2015-05-07 19:56:46 +0900

    change local list

--- controller/project.php
+++ controller/project.php
@@ -309,7 +309,7 @@ namespace Goteo\Controller {
 
 //                    $viewData['currently'] = Model\Project::currentStatus();
 //                    $viewData['scope'] = Model\Project::scope();
-                    $viewData['project_location'] = Model\Project::yokohamaLocation();
+                    $viewData['project_location'] = Model\Project::kitaqLocation();
                     break;
 
                 case 'costs':
