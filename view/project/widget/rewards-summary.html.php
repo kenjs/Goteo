b421037621399bae173f5f6c58575bd6cfb58317
Naoko Usui <usui@info-lounge.jp>
2015-05-26 16:23:42 +0900

    [common] reflect feedback(v12)

--- view/project/widget/rewards-summary.html.php
+++ view/project/widget/rewards-summary.html.php
@@ -47,15 +47,17 @@ uasort($project->individual_rewards,
 
     <h<?php echo $level ?> class="supertitle"><?php echo Text::get('project-rewards-supertitle'); ?></h<?php echo $level ?>>
 
-    <?php if (!empty($project->individual_rewards)) : ?>
+    <?php if (!empty($project->individual_rewards)) :
+        $count = 1;
+    ?>
     <div class="individual">
-        <h<?php echo $level+1 ?> class="title"><?php echo Text::get('project-rewards-individual_reward-title'); ?></h<?php echo $level+1 ?>>
+        <h<?php echo $level+1 ?> id="individual_ttl" class="title"><?php echo Text::get('project-rewards-individual_reward-title'); ?></h<?php echo $level+1 ?>>
         <ul>
         <?php foreach ($project->individual_rewards as $individual) : ?>
         <li class="<?php echo $individual->icon ?>">
 
             <div class="amount"><?php echo Text::get('regular-investing'); ?> <span class="euro"><?php echo \amount_format($individual->amount); ?>円</span></div>
-            <h<?php echo $level + 2 ?> class="name"><?php echo htmlspecialchars($individual->reward) ?></h<?php echo $level + 2 ?>>
+            <h<?php echo $level + 2 ?> id="<? echo 'individual_num' . $count; ?>" class="name"><?php echo htmlspecialchars($individual->reward) ?></h<?php echo $level + 2 ?>>
             <p><?php echo htmlspecialchars($individual->description)?></p>
 
                     <?/*php if (!empty($individual->units)) : ?>
@@ -70,22 +72,25 @@ uasort($project->individual_rewards,
             </div>
 
         </li>
+            <? $count++; ?>
         <?php endforeach ?>
         </ul>
     </div>
     <?php endif; ?>
 
-    <?php if (!empty($project->social_rewards)) : ?>
+    <?php if (!empty($project->social_rewards)) :
+        $count = 1;
+    ?>
         <div class="social">
-            <h<?php echo $level + 1 ?> class="title"><?php echo Text::get('project-rewards-social_reward-title'); ?></h<?php echo $level + 1 ?>>
+            <h<?php echo $level + 1 ?> id="social_ttl" class="title"><?php echo Text::get('project-rewards-social_reward-title'); ?></h<?php echo $level + 1 ?>>
             <ul>
                 <?php foreach ($project->social_rewards as $social) : ?>
                     <li class="<?php echo $social->icon ?>">
-                        <h<?php echo $level + 2 ?> class="name"><?php echo htmlspecialchars($social->reward) ?></h<?php echo $level + 2 ?>
+                        <h<?php echo $level + 2 ?> id="<? echo 'social_num' . $count; ?>" class="name"><?php echo htmlspecialchars($social->reward) ?></h<?php echo $level + 2 ?>
                         <p><?php echo htmlspecialchars($social->description)?></p>
                         <?php if (!empty($social->license) && array_key_exists($social->license, $licenses)): ?>
                             <div class="license <?php echo htmlspecialchars($social->license) ?>">
-                                <h<?php echo $level + 3 ?>><?php echo Text::get('regular-license'); ?></h<?php echo $level + 3 ?>>
+                                <h<?php echo $level + 3 ?> id="<? echo 'individual_num' . $count; ?>" ><?php echo Text::get('regular-license'); ?></h<?php echo $level + 3 ?>>
                                 <a href="<?php echo htmlspecialchars($licenses[$social->license]->url) ?>" target="_blank">
                                     <strong><?php echo htmlspecialchars($licenses[$social->license]->name) ?></strong>
 
@@ -96,6 +101,7 @@ uasort($project->individual_rewards,
                             </div>
                         <?php endif ?>
                     </li>
+                    <? $count++; ?>
                 <?php endforeach; ?>
             </ul>
         </div>
