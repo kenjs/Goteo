7492aaa890b1d25d878a0f6dd200e04b9958fd65
Naoko Usui <usui@info-lounge.jp>
2015-05-27 10:54:17 +0900

    [common] add to_top_link for PC view

--- view/js/localgood.js
+++ view/js/localgood.js
@@ -15,5 +15,15 @@ $(function(){
         }
     );
 
+    // smooth scroll
+    $('a[href^=#]').click(function(e){
+        e.preventDefault();
+        var speed = 350;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+    });
 
 });
