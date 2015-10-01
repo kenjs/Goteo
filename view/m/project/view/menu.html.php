a51327c0f781928517de1b3ff4dfb0a01743fb88
Masaki Ike <ike@info-lounge.jp>
2015-10-01 18:41:13 +0900

    [common] prepare for project evaluation implement

--- view/m/project/view/menu.html.php
+++ view/m/project/view/menu.html.php
@@ -28,8 +28,9 @@ use Goteo\Library\Text; ?>
             'needs'       => Text::get('project-menu-needs'),
             'supporters'  => Text::get('project-menu-supporters').' <span class="digits">'.'('.count($this['project']->investors).')'.'</span>',
             'messages'    => Text::get('project-menu-messages').' <span class="digits">'.$this['messages'].'</span>',
-            'updates'     => Text::get('project-menu-updates').' <span class="digits">'.$this['updates'].'</span>'
-        ) as $id => $show): ?>        
+            'updates'     => Text::get('project-menu-updates').' <span class="digits">'.$this['updates'].'</span>',
+            'evaluation'  => Text::get('project-menu-evaluation').' <span class="digits">'.$this['evaluation'].'</span>'
+        ) as $id => $show): ?>
         <li class="item <?php echo $id ?><?php if ($this['show'] == $id) echo ' show' ?>">
         	<a href="/project/<?php echo htmlspecialchars($this['project']->id) ?>/<?php echo $id ?>"><?php echo $show ?></a>
         </li>
